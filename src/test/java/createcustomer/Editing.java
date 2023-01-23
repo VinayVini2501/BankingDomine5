bcc375bb207a2f2b35998a0fcda48cd72ba5ff61
Vinay V <Vinay V@DESKTOP-DRH83U0>
2023-01-23 12:43:10 +0530

    1New commit

---
+++ src/test/java/createcustomer/Editing.java
@@ -0,0 +1,13 @@
+package createcustomer;
+
+import org.testng.annotations.Test;
+
+public class Editing
+{
+	@Test(groups= {"Smoke"})
+	public void edited()
+	{
+		System.out.println("Project edited");
+	}
+
+}
---
+++ src/test/java/createcustomer/Testing.java
@@ -0,0 +1,13 @@
+package createcustomer;
+
+import org.testng.annotations.Test;
+
+public class Testing 
+{
+	@Test(groups= {"regression"})
+	public void tested()
+	{
+		System.out.println("Complete testing");
+	}
+
+}
